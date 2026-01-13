4642baa24903fa5d9735454d15bb66e2c226f3c4
Faheem-Lateef <faheem.ltst@gmail.com>
2026-01-13 14:13:16 +0500

    Added new file

---
+++ client/pages/products/bottoms/trending.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { Breadcrumb, Col, Row } from 'react-bootstrap';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Loader from '../../../components/common/Loader';
+import Product from '../../../components/home/Product';
+import useWindowSize from '../../../hooks/useWindowSize';
+
+const BottomsTrending = ({ products, currentUser }) => {
+  const [loading, setLoading] = useState(true);
+  const [onMobile, setOnMobile] = useState(false);
+
+  const { width } = useWindowSize();
+
+  const filteredProducts = products?.filter((product) => product.category === 'Bottom');
+
+  useEffect(() => {
+    if (width <= 576) {
+      setOnMobile(true);
+    } else {
+      setOnMobile(false);
+    }
+
+    if (products) {
+      setLoading(false);
+    }
+  }, [width, products]);
+
+  return (
+<>
+<Head>
+<title>Trending Bottoms | Aurapan</title>
+</Head>
+{loading
+  ? (
+<div
+className="d-flex justify-content-center align-items-center px-0"
+style={{ marginTop: '80px' }}
+>
+<Loader />
+</div>
+    )
+  : (
+<>
+<h1 className="category-header">Trending Bottoms</h1>
+<Breadcrumb className="breadcrumb-label">
+<Link href="/" passHref>
+<Breadcrumb.Item>Home</Breadcrumb.Item>
+</Link>
+
+<Link href="/products/bottoms" passHref>
+<Breadcrumb.Item>Bottoms</Breadcrumb.Item>
+</Link>
+
+<Link href="/products/bottoms/trending" passHref>
+<Breadcrumb.Item>Trending</Breadcrumb.Item>
+</Link>
+</Breadcrumb>
+
+<Row className="mx-0">
+{filteredProducts.map((item) => (
+<Col key={item.id} xs={6} md={4} xl={3} className="p-0">
+<Product
+onMobile={onMobile}
+product={item}
+currentUser={currentUser}
+/>
+</Col>
+))}
+</Row>
+</>
+    )}
+</>
+  );
+};
+
+export default BottomsTrending;
